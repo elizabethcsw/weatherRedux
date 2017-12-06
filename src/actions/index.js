6e94519e805205ef77349dfb1dcc5ef7127d5996
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-12-06 17:20:48 +0000

    added axios

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,15 @@
+import axios from 'axios';
 const API_KEY = 'd33204696ff28be3ace87e617b5cad88'
+const rootURL = `api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+export const FETCH_WEATHER = "FETCH_WEATHER"
 
 
-export function selectBook(book){
-  console.log("a book has been selected: ", book.title)
+export function fetchWeather(){
+  const url = `${rootURL}&q=${city},us`
+  const request = axios.get(url);
+
   return {
-    type: "BOOK_SELECTED",
-    payload: book
+    type: FETCH_WEATHER,
+    payload: request
   };
 }
