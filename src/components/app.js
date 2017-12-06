ad73adc5995c6c688fbcc5f75045e2ec5d2cfb8b
Elizabeth Chan <elizabeth.schan@outlook.com>
2017-12-06 16:38:19 +0000

    simple search bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import BookList from '../containers/book_list'
 import BookDetail from '../containers/book_detail'
+import SearchBar from '../containers/search_bar'
 
 export default class App extends Component {
   render() {
     return (
       <div>Hello there
-        <BookList />
-        <BookDetail />
+        <SearchBar />
       </div>
 
     );
---
+++ src/containers/search_bar.js
@@ -0,0 +1,13 @@
+import React, { Component} from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+export default class SearchBar extends Component {
+  render(){
+    return(
+      <form className="input-group">
+        <input />
+      </form>
+    )
+  }
+}
